ea31a0253088348e93503fd8641602e86f8f4f8a
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-14 17:33:53 +0100

    update

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,17 +4,20 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import {QueryClient, QueryClientProvider} "@tanstack/react-query"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
-  <QueryClientProvider client={QueryClient}>
-  <App />
-  </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
