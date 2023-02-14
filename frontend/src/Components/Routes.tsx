6b99743cc9665d5cd28b1db4214d5fa268372f20
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-14 17:54:50 +0100

    update

--- frontend/src/Components/Routes.tsx
+++ frontend/src/Components/Routes.tsx
@@ -2,8 +2,38 @@ import React from "react";
 import { useRoutes } from "react-router";
 import DetailPost from "./DetailPost";
 import PostData from "./PostData";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { CreatingPostData, FetchPost } from "./Api";
+import { Link } from "react-router-dom";
 
 const Routes = () => {
+  const queryClient = useQueryClient();
+
+  const [title, setTitle] = React.useState("");
+  const [description, setDescription] = React.useState("");
+
+  const getPost = useQuery({
+    queryKey: ["post"],
+    queryFn: FetchPost,
+  });
+
+  const posting = useMutation({
+    mutationFn: CreatingPostData,
+
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(["post"]);
+    },
+  });
+
+  const handleSubmit = () => {
+    posting.mutate({
+      myTitle: title,
+      description,
+    });
+  };
+
+  console.log(getPost);
+
   const element = useRoutes([
     {
       path: "/",
