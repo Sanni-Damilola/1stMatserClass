37be6b05f495b3b5452290de26ce4a0f66deb8c1
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-14 17:32:33 +0100

    update

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,15 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-// import {} ""
+import {QueryClient, QueryClientProvider} "@tanstack/react-query"
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
+  <BrowserRouter>
+  <QueryClientProvider client={QueryClient}>
+  <App />
+  </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
