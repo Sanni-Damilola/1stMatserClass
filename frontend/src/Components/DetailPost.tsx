66533356f5ce56f6a1f190cae7b660dd30741adf
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-14 17:53:48 +0100

    update

--- frontend/src/Components/DetailPost.tsx
+++ frontend/src/Components/DetailPost.tsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { FetchPost, FetchSinglePost } from "./Api";
 
 const DetailPost = () => {
+  const { id } = useParams();
+
+  console.log(id);
+
+  const fetchData = useQuery({
+    queryKey: ["post", id],
+    queryFn: () => FetchSinglePost(id),
+  });
+
   return (
     <div>
-      DetailPost
+      <div>
+        <h1> Detailed</h1>
+        <br />
+        <br />
+        <br />
+        <br />
+        {fetchData.isLoading ? <h3>Loading..</h3> : null}
+        <div>{fetchData.data?.myTitle}</div>
+        <div>{fetchData.data?.description}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DetailPost
+export default DetailPost;
