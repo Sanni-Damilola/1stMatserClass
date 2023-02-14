ffdc3f82fd6477ed250a70a83c3485498b1ffc39
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-14 17:56:42 +0100

    update

--- frontend/src/Components/PostData.tsx
+++ frontend/src/Components/PostData.tsx
@@ -4,24 +4,92 @@ import { CreatingPostData, FetchPost } from "./Api";
 import { Link } from "react-router-dom";
 
 const PostData = () => {
+  const queryClient = useQueryClient();
+
+  const [title, setTitle] = React.useState("");
+  const [description, setDescription] = React.useState("");
+
+  const getPost = useQuery({
+    queryKey: ["post"],
+    queryFn: FetchPost,
+  });
+
+  const posting = useMutation({
+    mutationFn: CreatingPostData,
+
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(["post"]);
+    },
+  });
+
+  const handleSubmit = () => {
+    posting.mutate({
+      myTitle: title,
+      description,
+    });
+  };
+
   return (
     <div>
       <br />
       <br />
-      <br />
-      <br />
       <h1>Create Post</h1>
       <input
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
         placeholder="title"
-        style={{ height: "30px", width: "300px", margin: "10px" }}
-        type="text"
-      />{" "}
+        style={{
+          height: "30px",
+          width: "300px",
+          margin: "10px",
+        }}
+      />
       <br />
       <textarea
-        placeholder="add description"
-        style={{ height: "100px", width: "300px", margin: "10px" }}
-      ></textarea>
-      <h2>All Post</h2>
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+        placeholder="description"
+        style={{
+          height: "100px",
+          width: "300px",
+          margin: "10px",
+        }}
+      />
+      <br />
+      <button disabled={posting?.isLoading} onClick={handleSubmit}>
+        Add Post
+      </button>
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h2>All Posts</h2>
+
+      <div>
+        {getPost.isLoading ? <h1>Loading...</h1> : null}
+        {getPost?.data?.map((props: any) => (
+          <Link to={`/detail/${props._id}`}>
+            <div
+              key={props?._id}
+              style={{
+                height: "70px",
+                width: "300px",
+                background: "#f1f1f1",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                margin: "10px",
+                border: "1px solid silver",
+              }}
+            >
+              <div>{props?.myTitle}</div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
