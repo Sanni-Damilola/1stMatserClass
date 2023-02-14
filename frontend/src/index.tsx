3526a08e90a974705e159a76cfd426cead428795
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-14 17:36:29 +0100

    update

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools/build/lib/devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
